725e65644fe8c24413f52ee218fc18340d095508
nathanieluriri <uririnathaniel@gmail.com>
2025-04-20 23:19:45 +0100

    added summary

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI, Depends
 from api.v1 import user,book,bookmark,like,chapter,page
 from security.auth import verify_token
-app = FastAPI(title="Modular FastAPI",servers=[
-    {"url": "http://127.0.0.1:8000/", "description": "Local development server"},
+app = FastAPI(title="Mei FastAPI Backend",
+              servers=
+    [
+        {"url": "http://127.0.0.1:8000/", "description": "Local development server"},
         {"url": "https://api.example.com", "description": "Production server"},
         {"url": "http://127.0.0.1:8000/", "description": "Sandbox server"},
         
-    ])
+    ],
+    summary="hello"
+    
+    )
 
 app.include_router(user.router, prefix="/api/v1/user", tags=["User"],dependencies=[Depends(verify_token)])
 app.include_router(book.router,prefix="/api/v1/book", tags=["Book"])
