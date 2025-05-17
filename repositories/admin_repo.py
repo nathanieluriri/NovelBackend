efcc15a9c7bb248b3e4ca162d2d2fa486811f5fe
nathanieluriri <uririnathaniel@gmail.com>
2025-05-17 12:34:07 +0100

    added register admin feature

--- repositories/admin_repo.py
+++ repositories/admin_repo.py
@@ -20,3 +20,23 @@ async def create_admin(user_data: NewAdminCreate):
 
 async def delete_admin_by_email_and_provider(email: str,provider:str):
     return await db.admins.delete_one({"email": email})
+
+
+
+async def get_allowd_admin_emails(email: str):
+    admin = await db.AllowedAdmins.find_one({"email": email})
+    try:
+        newAdmin = NewAdminOut(**admin)
+        return newAdmin
+    except TypeError:
+        print("this email isn't allowed to register the email")
+        return None
+        
+        
+        
+async def create_email_list_for_admins(email: str):
+    user_data = {}
+    user_data['email']=email
+    result = await db.AllowedAdmins.insert_one(user_data)
+    created_user = await db.AllowedAdmins.find_one({"_id": result.inserted_id})
+    return created_user
--- services/admin_services.py
+++ services/admin_services.py
@@ -1,4 +1,4 @@
-from repositories.admin_repo import get_admin_by_email, create_admin,delete_admin_by_email_and_provider
+from repositories.admin_repo import get_admin_by_email, create_admin,delete_admin_by_email_and_provider, get_allowd_admin_emails
 from schemas.admin_schema import NewAdminCreate,NewAdminOut,NewAdminBase
 from fastapi import HTTPException,status
 from security.hash import check_password
@@ -8,21 +8,25 @@ from security.admin_otp import generate_otp,send_otp
 
 
 async def register_admin_func(user_data: NewAdminCreate):
-    existing = await get_admin_by_email(user_data.email)
-    if existing:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT,detail="User already exists")
-    new_user =await create_admin(user_data)
-    print(new_user)
-    new_user = NewAdminOut(**new_user)
-    accessToken= await generate_admin_access_tokens(new_user.userId)
-    new_user.accessToken=accessToken.accesstoken
-    otp = generate_otp(admin_access_token=new_user.accessToken)
-    send_otp(otp=otp)
-    refreshToken= await generate_refresh_tokens(userId=new_user.userId,accessToken=new_user.accessToken)
-    
-    
-    new_user.refreshToken=refreshToken.refreshtoken
-    return new_user
+    if get_allowd_admin_emails(user_data.email)!=None:
+        existing = await get_admin_by_email(user_data.email)
+        if existing:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT,detail="User already exists")
+        new_user =await create_admin(user_data)
+        print(new_user)
+        new_user = NewAdminOut(**new_user)
+        accessToken= await generate_admin_access_tokens(new_user.userId)
+        new_user.accessToken=accessToken.accesstoken
+        # generate and send otp
+        otp = generate_otp(admin_access_token=new_user.accessToken)
+        send_otp(otp=otp)
+        refreshToken= await generate_refresh_tokens(userId=new_user.userId,accessToken=new_user.accessToken)
+        
+        
+        new_user.refreshToken=refreshToken.refreshtoken
+        return new_user
+    else:
+        return None
 
 
 
@@ -38,6 +42,7 @@ async def login_admin_func(user_data:NewAdminBase):
                 
                 accessToken=await generate_admin_access_tokens(str(existing['_id']))
                 existing['accessToken']= accessToken.accesstoken
+                # generate and send otp 
                 otp = generate_otp(admin_access_token=accessToken.accessToken)
                 send_otp(otp=otp)
                 
