5c93a9c610ab7cf4ed972c21742b53d7ab170e59
nathanieluriri <uririnathaniel@gmail.com>
2025-07-07 03:42:46 +0100

    made the user update whatever to fit josh requirements

--- services/admin_services.py
+++ services/admin_services.py
@@ -181,7 +181,7 @@ async def get_one_user_details(userId:str):
         list_of_unlocked_chapters =user['unlockedChapters']
         chapterDetailsList=[]
         for chapter in list_of_unlocked_chapters:
-            chapterDetails = await get_chapter_by_chapter_id(chapterDetails)
+            chapterDetails = await get_chapter_by_chapter_id(chapter)
             if chapterDetails:
                 chapterDetails['hasRead']=True # TODO: Here you will replace with db logic that checks if this user has read this chapter
                 chapterDetailsList.append(chapterDetailsList)
