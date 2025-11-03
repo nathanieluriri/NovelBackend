8d7af4758920b63cf3502d7b8972c58fac40c4d7
nathanieluriri <uririnathaniel@gmail.com>
2025-11-04 00:36:15 +0100

    automated commit

--- services/admin_services.py
+++ services/admin_services.py
@@ -69,7 +69,7 @@ async def login_admin_func(user_data:AdminBase,location:ClientData):
                 otp = generate_otp(admin_access_token=existing["accessToken"])
                 print("otp",otp)
                 
-                # await send_otp(otp=otp,location=location,user_email=user_data.email)
+                await send_otp(otp=otp,location=location,user_email=user_data.email)
                 # TODO: Probably switch send_otp to something
                 
                 refreshToken=await generate_refresh_tokens(userId=str(existing['_id']),accessToken=accessToken.accesstoken)
